b395cb9378ef92485ccb2e2bf0db1dc680b5365b
Joey Singer <joeysinger7@gmail.com>
2017-02-09 19:28:38 -0500

    Fixed Deletion of Original Files

---
+++ Tumblr API/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Tumblr API
+//
+//  Created by Joey Singer on 2/8/17.
+//  Copyright © 2017 Joey Singer. All rights reserved.
+//
+
+import Foundation
---
+++ Tumblr API/PhotoCell.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoCell.swift
+//  Tumblr API
+//
+//  Created by Joey Singer on 2/8/17.
+//  Copyright © 2017 Joey Singer. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+
+
+}
